46450528e082488c8094fb029afeea936d7fc179
shengda567 <57602285+shengda567@users.noreply.github.com>
2020-10-01 18:56:43 -0400

    commit on 'master'

---
+++ src/axios/index.js
@@ -0,0 +1,12 @@
+// import JsonP from 'jsonp'
+// export default class Axios{
+//     rew static jsonp(){
+//         new Promise((resolve, reject) => {
+//             JsonP(options.url, {
+//                 parem: 'calllback'
+//             }, function (err, response){
+                
+//             })
+//         })
+//     }
+// }
