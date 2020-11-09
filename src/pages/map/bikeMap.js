bd43668f3da105816f14583bb6c58d232896d826
JWSolo <49379304+JWSolo@users.noreply.github.com>
2020-11-09 13:44:39 -0500

    MapBox Api

--- src/pages/map/bikeMap.js
+++ src/pages/map/bikeMap.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Card, Form } from 'antd'
 import axios from '../../axios/index'
 import BaseForm from '../../components/BaseForm'
+import mapboxgl, { Marker } from 'mapbox-gl'
+import '../map/bikeMap.css'
 export default class Order extends React.Component{
 
     state = {
@@ -34,7 +36,7 @@ export default class Order extends React.Component{
     // 列表请求
     requestList = ()=>{
         axios.ajax({
-            url:'/bikemap',
+            url:'/backup',
             data:{
                 params:this.params
             }
@@ -63,89 +65,199 @@ export default class Order extends React.Component{
     // 渲染地图
     renderMap = (res) => {
         let list = res.route_list;
-        this.map = new window.BMap.Map("container", {enableMapClick: false});
-        let gps1 = list[0].split(',');
-        let startPoint = new window.BMap.Point(gps1[0], gps1[1]);
-        let gps2 = list[list.length - 1].split(',');
-        let endPoint = new window.BMap.Point(gps2[0], gps2[1]);
-
-        this.map.centerAndZoom(endPoint, 11);
-        // map.clearOverlays();
-
         //添加起始图标
-        let startPointIcon = new window.BMap.Icon("/assets/start_point.png", new window.BMap.Size(36, 42), {
-            imageSize: new window.BMap.Size(36, 42),
-            anchor: new window.BMap.Size(18, 42)
+        var geojson = {
+            type: 'FeatureCollection',
+            features: [{
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [-74.023733, 40.745183]
+              },
+              properties: {
+                title: 'Bike loaction',
+                description: 'Howe Center Lot'
+              }
+            },
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [-74.027554, 40.734979]
+              },
+              properties: {
+                title: 'Bike loaction',
+                description: 'Hoboken Terminal'
+              }
+            },
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [-74.033787, 40.741748]
+              },
+              properties: {
+                title: 'Bike loaction',
+                description: 'Hoboken University Medical Center'
+              }
+            },            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [-74.042781,40.741527]
+              },
+              properties: {
+                title: 'Bike loaction',
+                description: '2nd Street Light Rail Staion'
+              }
+            },
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [-74.033811, 40.727030]
+              },
+              properties: {
+                title: 'Bike loaction',
+                description: 'Newport Path Station'
+              }
+            },
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [-74.042737,40.719553]
+              },
+              properties: {
+                title: 'Bike loaction',
+                description: 'Grove Street Path Station'
+              }
+            },
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [-74.052860, 40.719185]
+              },
+              properties: {
+                title: 'Bike loaction',
+                description: '99 Ranch Market'
+              }
+            },
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [-74.035943,40.732875]
+              },
+              properties: {
+                title: 'Bike loaction',
+                description: 'Target'
+              }
+            }]
+          };
+
+        mapboxgl.accessToken = 'pk.eyJ1IjoiamFja3NvbG8iLCJhIjoiY2toNnBwMXRjMDBubTJycGQyZ3FrbHhnYiJ9.ncXh5O5Yi17CBF2cFKn3iA';
+        var map = new mapboxgl.Map({
+            container:'map',
+            center: [-74.027554, 40.734979],
+            style: 'mapbox://styles/jacksolo/ckh6qmrra040d19qe44v4wi6w',
+            zoom: 13
         });
+
+        geojson.features.forEach(function(marker) {
+
+            // create a HTML element for each feature
+            var el = document.createElement('div');
+            el.className = 'marker';
+          
+            // make a marker for each feature and add to the map
+            new mapboxgl.Marker(el)
+                .setLngLat(marker.geometry.coordinates)
+                .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+                .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+                .addTo(map);
+          });
+        /*
+        var marker = new mapboxgl.Marker()
+                .setLngLat([-74.024827,40.745500])
+                .addTo(this.map);
+        */
         
-        var bikeMarkerStart = new window.BMap.Marker(startPoint, { icon: startPointIcon });
-        this.map.addOverlay(bikeMarkerStart);
-
-        let endPointIcon = new window.BMap.Icon("/assets/end_point.png", new window.BMap.Size(36, 42), {
-            imageSize: new window.BMap.Size(36, 42),
-            anchor: new window.BMap.Size(18, 42)
-        });
-        var bikeMarkerEnd = new window.BMap.Marker(endPoint, { icon: endPointIcon });
-        this.map.addOverlay(bikeMarkerEnd);
-
-        let routeList = [];
-        list.forEach((item)=>{
-            let p = item.split(",");
-            let point = new window.BMap.Point(p[0], p[1]);
-            routeList.push(point);
-        })
         // 行驶路线
-        var polyLine = new window.BMap.Polyline(routeList, {
-            strokeColor: "#ef4136",
-            strokeWeight: 3,
-            strokeOpacity: 1
-        });
-        this.map.addOverlay(polyLine);
+        map.on('load', function () {
+          map.addSource('route', {
+              'type': 'geojson',
+              'data': {
+                  'type': 'Feature',
+                  'properties': {},
+                  'geometry': {
+                      'type': 'LineString',
+                      'coordinates': [
+                          [-74.042737,40.719553],
+                          [-74.035943,40.732875]
+                      ]
+                  }
+              }
+          });
+          map.addLayer({
+              'id': 'route',
+              'type': 'line',
+              'source': 'route',
+              'layout': {
+                  'line-join': 'round',
+                  'line-cap': 'round'
+              },
+              'paint': {
+                  'line-color': '#088',
+                  'line-width': 8
+              }
+          });
+      });
+
 
         // 服务区路线
-        let serviceList = res.service_list;
-        let servicePointist = [];
-        serviceList.forEach((item) => {
-            let point = new window.BMap.Point(item.lon, item.lat);
-            servicePointist.push(point);
-        })
-        // 画线
-        var polyServiceLine = new window.BMap.Polyline(servicePointist, {
-            strokeColor: "#ef4136",
-            strokeWeight: 3,
-            strokeOpacity: 1
-        });
-        this.map.addOverlay(polyServiceLine);
+        map.on('load', function () {
+          map.addSource('maine', {
+              'type': 'geojson',
+              'data': {
+                  'type': 'Feature',
+                  'geometry': {
+                      'type': 'Polygon',
+                      'coordinates': [
+                          [
+                            [-74.010777, 40.777223],//port imperial
+                            [-74.031356, 40.787589],//tonnelle ave station
+                            [-74.082462, 40.725370],//lincoln park
+                            [-74.033762, 40.712222]//exchange place
+                          ]
+                      ]
+                  }
+              }
+          });
+          map.addLayer({
+              'id': 'maine',
+              'type': 'fill',
+              'source': 'maine',
+              'layout': {},
+              'paint': {
+                  'fill-color': '#048',
+                  'fill-opacity': 0.4
+              }
+          });
+      });
+  
 
         // 添加地图中的自行车
-        let bikeList = res.bike_list;
-        let bikeIcon = new window.BMap.Icon("/assets/bike.jpg", new window.BMap.Size(36, 42), {
-            imageSize: new window.BMap.Size(36, 42),
-            anchor: new window.BMap.Size(18, 42)
-        });
-        bikeList.forEach((item) => {
-            let p = item.split(",");
-            let point = new window.BMap.Point(p[0], p[1]);
-            var bikeMarker = new window.BMap.Marker(point, { icon: bikeIcon });
-            this.map.addOverlay(bikeMarker);
-        })
+
         
         // 添加地图控件
-        this.addMapControl();
+     
     };
 
-    // 添加地图控件
-    addMapControl = () => {
-        let map = this.map;
-        // 左上角，添加比例尺
-        var top_right_control = new window.BMap.ScaleControl({anchor: window.BMAP_ANCHOR_TOP_RIGHT});
-        var top_right_navigation = new window.BMap.NavigationControl({anchor: window.BMAP_ANCHOR_TOP_RIGHT});
-        //添加控件和比例尺
-        map.addControl(top_right_control);
-        map.addControl(top_right_navigation);
-        map.enableScrollWheelZoom(true);
-        // legend.addLegend(map);
-    };
+
+
+
 
     render(){
         return (
@@ -155,7 +267,7 @@ export default class Order extends React.Component{
                 </Card>
                 <Card style={{marginTop:10}}>
                     <div>Total numeber of bikes: {this.state.total_count}</div>
-                    <div id="container" style={{height:500}}></div>
+                    <div id="map" style={{height:500,width: 1400, marginTop:100}}></div>
                 </Card>
             </div>
         );
